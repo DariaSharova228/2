931ed3818fc5cea51d79160e4b4ade27bebb2d33
DariaSharova228 <dariasharova02@gmail.com>
2022-11-09 14:55:34 +0300

    seq

--- sequence.h
+++ sequence.h
@@ -4,18 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
+int f1(const char* name, int* count, double* sum);
+int f2(const char* name, double min, int* count);
 void *thread_func(void *ptr);
 
+void reduce_sum(int p, double* a = nullptr, int n = 0);
+
+class Global_Results{
+    public:
+    int status = -100;
+    int count = 0;
+    int answer = 0;
+};
 class Results {
     public:
         int status = -100; //not opened
         int count = 0; //when status >= 0
+        double sum = 0;
 };
 
 class Args {
     public:
         Results *res = nullptr; //length = p
+        Global_Results *Res = nullptr;
         int k = 0; //k thread
         int p = 0; //number of threads
         char *name = nullptr; //filename
