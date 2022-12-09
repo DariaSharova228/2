fecc2420a8c7b523c8d7cc8f747ca843c29e32df
DariaSharova228 <dariasharova02@gmail.com>
2022-12-10 02:27:35 +0300

    seq

--- sequence.cpp
+++ sequence.cpp
@@ -12,7 +12,8 @@ int read_arr(double *a, int n, char *name) {
     }
 
     for(i = 0; i < n; i++) {
-        if (fscanf(inp, "%lf", a + i) != 1) {
+        a[i] = 0;
+        if (fscanf(inp, "%lf", &a[i]) != 1) {
             fclose(inp);
             return -2;
         }
@@ -20,6 +21,14 @@ int read_arr(double *a, int n, char *name) {
 
     fclose(inp);
     return 0;
+}
+void print_arr(double *a, int n1, int n2) {
+    int i = 0, j = 0;
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++)
+            printf("%lf ", a[i * n2 + j]);
+        printf("\n");
+    }
 }
 void reduce_sum(int p, double* a, int n){
   static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
@@ -63,334 +72,189 @@ void reduce_sum(int p, double* a, int n){
   }
   pthread_mutex_unlock(&m);
 }
-/*int f1(const char* name, int* count, double* sum1) {// подсчет локальных минимумов
-    FILE *in;
-    double curr, next, prev;
-    int quantity = 0;
-    double s = 0;
-    if(!(in = fopen(name, "r"))) {
-        return -1;
-    }
-    if(fscanf(in,"%lf",&prev)!=1 && (!feof(in))){
-        fclose(in);
-        return -2;
-    }
-    if(fscanf(in,"%lf",&curr)!=1 && (!feof(in))){
-        fclose(in);
-        return -2;
-    }
-    while(fscanf(in,"%lf",&next) == 1) {
-        if(curr < prev && curr < next) {
-            s += curr;
-            quantity++;
-        }
-        prev = curr;
-        curr = next;
-    }
-    if (!feof(in)) 
-    {
-        fclose(in);
-        return -2;
-    }
-    fclose(in);
-    *count += quantity;
-    *sum1 += s;
-    return 0;
-
-}
-int f2(const char* name, double min, int* count){// подсчет числа элементов
-    FILE *in;
-    double curr;
-    int sum1 = 0;
-    if(!(in = fopen(name, "r"))) {
-        return -1;
-    }
-    while (fscanf(in, "%lf", &curr) == 1)
-    {
-      if (curr < min)
-        {
-          sum1++;
-        }
-    }
-    if (!feof(in))
-    {
-      fclose(in);
-      return -2;
-    }
-    fclose(in);
-    *count = sum1;
-    return 0;
-}*/
-/*double get_full_time()
-{
-  struct timeval buf;
-  gettimeofday(&buf, NULL);
-  return buf.tv_sec + buf.tv_usec / 1.e6;
-}
-
-double get_CPU_time()
-{
-  struct rusage buf;
-  getrusage(RUSAGE_THREAD, &buf);
-  return buf.ru_utime.tv_sec + buf.ru_utime.tv_usec / 1.e6;
-}*/
-void full_array(double *arr, int n, int p, int k, Results *res) {
-    int l = n % p;
-    int lenk = n / p + (k < l ? 1 : 0);
+void check_arr(double *arr, double *sum, int *count, int n1, int n2, int p, int k) {
+    int l = n1 % p;
+    int lenk = n1 / p + (k < l ? 1 : 0);
     int first = (k < l ? k * lenk : k * lenk + l);
-    //printf("thr %d     %d\n", k, first);
-    
-    if (first) {
-        res[k].flagl1 = 1;
-        res[k].prev1 = arr[first - 1];
-    }
-    if(first - 1) {
-        res[k].flagl2 = 1;
-        res[k].prev2 = arr[first - 2];
-    }
-    if (first + lenk < n) {
-        res[k].flagr1 = 1;
-        res[k].next1 = arr[first + lenk];
-    }
-    if(first + lenk + 1< n) {
-        res[k].flagr2 = 1;
-        res[k].next2 = arr[first + lenk + 1];
-    }
-}
-int count_change(double *arr, int n, int p, int k, Results *res) {
-    int l = n % p;
-    int lenk = n / p + (k < l ? 1 : 0);
-    int first = (k < l ? k * lenk : k * lenk + l);
-    int count = 0;
-    double a = 0;
-    double prev = 0;
-    double pprev = 0;
     int i = 0;
-    if (p > n) return 0;
-    pprev = arr[first];
-    prev = arr[first + 1];
-    if(res[k].flagl1 == 1 && res[k].flagl2 == 1) {
-        if(lenk == 1) {
-            if(res[k].flagr1 == 1 && res[k].flagr2 == 1) {
-                a = res[k].prev1 * res[k].prev2 * res[k].next1 * res[k].next2;
-                if(a > 0 || (a - 0) < EPS) {
-                    arr[first] = pow(a, 1/4.0);
-                    return 1;
-                } 
-                return 0;
-            }
-            return 0;
-        }
-        if(lenk == 2) {
-            if(res[k].flagr1 == 1) {
-                a = res[k].prev1 * res[k].prev2 * arr[first + 1] * res[k].next1;
-                if(a > 0 || (a - 0) < EPS) {
-                    pprev = arr[first];
-                    arr[first] = pow(a, 1/4.0);
-                    count++;
-                }
-                if(res[k].flagr2 == 1) {
-                    a = res[k].prev1 * pprev * res[k].next1 * res[k].next2;
-                    if(a > 0 || (a - 0) < EPS) {
-                        arr[first + 1] = pow(a, 1/4.0);
-                        count++;
-                        return count;
-                    }
-                    return count;
-                }
-                return count;
-            }
-            return 0;
-        }
-        if(lenk == 3) {
-            a = res[k].prev1 * res[k].prev2 * arr[first + 1] * arr[first + 2];
-            if(a > 0 || (a - 0) < EPS) {
-                pprev = arr[first];
-                arr[first] = pow(a, 1/4.0);
-                count++;
-                printf("HERE\n");
-            }
-            if(res[k].flagr1 == 1){
-                a = res[k].prev1 * pprev * arr[first + 2] * res[k].next1;
-                if(a > 0 || (a - 0) < EPS) {
-                    prev = arr[first + 1];
-                    arr[first + 1] = pow(a, 1/4.0);
-                    count++;
-                }
-            }
-            if(res[k].flagr2 == 1) {
-                a = pprev * prev * res[k].next1 * res[k].next2;
-                if(a > 0 || (a - 0) < EPS) {
-                    pprev = prev;
-                    prev = arr[first + 2];
-                    arr[first + 2] = pow(a, 1/4.0);
-                    count++;
-                }
-            }
-            return count;
-        }
-        if(lenk > 3) {
-            a = res[k].prev1 * res[k].prev2 * arr[first + 1] * arr[first + 2];
-            if(a > 0 || fabs(a - 0) < EPS) {
-                pprev = arr[first];
-                arr[first] = pow(a, 1/4.0);
-                count++;
-            }
-            a = res[k].prev1 * pprev * arr[first + 2] * arr[first + 3];
-            if(a > 0 || fabs(a - 0) < EPS) {
-                prev = arr[first + 1];
-                arr[first + 1] = pow(a, 1/4.0);
-                count++;
+    int j = 0;
+    int h = (first + lenk < n1 ? first + lenk : n1);
+    double mean0 = 0;
+
+    for(i = first + 1; i < h - 1; i++) {
+        for(j = 1; j < n2 - 1; j++) {
+            mean0 = (arr[(i + 1) * n2 + j] + arr[i * n2 + j - 1] + arr[i * n2 + j + 1]  + arr[(i - 1) * n2 + j]) / 4.;
+            if ((arr[i * n2 + j]) > mean0 || (fabs(arr[i * n2 + j] - mean0) < EPS)) {
+                (*sum) += arr[i * n2 + j];
+                (*count)++;
             }
         }
     }
-    for(i = 2; i < lenk - 2; i++) {
-        a = pprev * prev * arr[first + i + 1] * arr[first + i + 2];
-        pprev = prev;
-        prev = arr[first + i];
-        if(a > 0 || fabs(a - 0) < EPS) {
-            arr[first + i] = pow(a, 1/4.0);
-            count++;
-        }
-    }
-    if(lenk == 3) {
-        if(res[k].flagr1 == 1 && res[k].flagr2 == 1){
-            a = pprev * prev * res[k].next1 * res[k].next2;
-            if(a > 0 || fabs(a - 0) < EPS) {
-            arr[first + 2] = pow(a, 1/4.0);
-            count++;
-            }
-        }
-        return count;
-    }
-    if(lenk > 3) {
-    if(res[k].flagr1 == 1) {
-        a = pprev * prev * arr[first + lenk - 1] * res[k].next1;
-        pprev = prev;
-        prev = arr[first + lenk - 2];
-        if(a > 0 || fabs(a - 0) < EPS) {
-            arr[first + lenk - 2] = pow(a, 1/4.0);
-            count++;
-        }
-        if(res[k].flagr2 == 1) {
-            a = pprev * prev * res[k].next1 * res[k].next2;
-            if(a > 0 || fabs(a - 0) < EPS) {
-                arr[first + lenk - 1] = pow(a, 1/4.0);
-                count++;
-            }
-        }
-    }
-    }
-    return count;
 }
+
+void change_arr(double *arr, double *arr1, double *arr2, double mean, int n1, int n2, int p, int k, Results *res) {
+    int l = n1 % p;
+    int lenk = n1 / p + (k < l ? 1 : 0);
+    int first = (k < l ? k * lenk : k * lenk + l);
+    int i = 0, i1 = 0;
+    int j = 0;
+    int h = (first + lenk < n1 ? first + lenk : n1);
+    double mean0 = 0;
+    //printf("thr %d     %d\n", k, first);
+    //printf("%d\n", h);
+    
+    for(i1 = 0; i1 < n2; i1++) {
+        arr1[i1] = arr[first * n2 + i1];
+    }
+
+    if (first + 1 < n1 - 1) {
+        for(i1 = 0; i1 < n2; i1++) {
+            arr2[i1] = arr[(first + 1) * n2 + i1];
+        }
+    }
+    res[k].arr2 = arr2;
+
+    for(i = first + 1; i < h - 1; i++) {
+        for(j = 1; j < n2 - 1; j++) {
+            //print_arr(arr1, 1, n2);
+            mean0 = (arr[(i + 1) * n2 + j] + arr1[j] + arr[i * n2 + j + 1]  + arr[i * n2 + j - 1]) / 4.;
+            //printf("\n%lf   %lf   %d %d\n\n\n", arr[i * n2 + j], mean0 * 5, i, j);
+            arr1[j] = arr[i * n2 + j];
+            if ((arr[i * n2 + j]) > mean0 || (fabs(arr[i * n2 + j] - mean0) < EPS)) {
+                arr[i * n2 + j] = mean;
+            }
+        }
+        arr1[n2 - 1] = arr[i * n2 + n2 - 1];
+    }
+    res[k].arr1 = arr1;
+    /*printf("--------------------------\n");
+    printf("--------------------------\n");
+    print_arr(arr1, 1, n2);
+    printf("--------------------------\n");
+    print_arr(arr2, 1, n2);
+    printf("--------------------------\n");
+    printf("--------------------------\n");*/
+}
+
 void* thread_func(void *ptr) {
-double time = 0;
-Args* args = (Args*) ptr;
-double* arr = args->arr;
-int n = args->n;
-int k = args->k;
-int p = args->p;
-Results *res = args->res;
-int countk = 0;
-if(n <= 4)
-{
-    return 0;
-}
-time = clock();
-full_array(arr, n, p, k, res);
-reduce_sum(p);
-countk = count_change(arr,n, p, k, res);
-reduce_sum(p);
-time = (clock() - time) / CLOCKS_PER_SEC;
-args -> count = countk;
-args -> time = time;
-return nullptr;
-}
-/*void* thread_func(void *ptr) {
     Args *a = (Args*) ptr;
     int k = a -> k;
     int p = a -> p;
-    char *name = a -> name;
-    Results *r = a -> res;
+    int n1 = a -> n1;
+    int n2 = a -> n2;
+    double *arr = a -> arr;
+    Results *res = a -> res;
+    double time = 0;
     int count = 0;
-    int err = 0;
-    double sum = 0;
-    double mean;
-    int i;
-    double w;
-    r[k].status = f1(name, &r[k].count, &r[k].sum);
-    reduce_sum(p);
-    err = 0;
-    for(i = 0; i < p; i++) {
-        if (r[i].status != 0) {
-            err++;
-            if (k == 0) {
-                switch (r[i].status) {
-                    case -1:
-                        printf("Cannot open file %s!\n", a[k].name);
-                        break;
-                    case -2:
-                        printf("Cannot read file %s!\n", a[k].name);
-                        break;
-                    default:
-                        printf("Unknown error! %s\n", a[k].name);
+    double mean0 = 0, mean = 0;
+    double *arr1 = nullptr, *arr2 = nullptr;
+    double *arr10 = nullptr, *arr20 = nullptr;
+    int l = n1 % p;
+    double val;
+    //int lenk = n1 / p + (k < l ? 1 : 0);
+    //int first = (k < l ? k * lenk : k * lenk + l);
+    int leni = 0, leni1 = 0, firsti = 0, lasti = 0;
+    int i = 0, j = 0;
+
+    arr1 = new double[n2];
+    if (!arr1) {
+        res[k].err = -1;
+        return nullptr;
+    }
+
+    arr2 = new double[n2];
+    if (!arr2) {
+        res[k].err = -1;
+        delete []arr1;
+        return nullptr;
+    }
+
+    for(i = 0; i < n2; i++) {
+        arr1[i] = 0;
+        arr2[i] = 0;
+    }
+    /*printf("--------------------------\n");
+    print_arr(arr1, 1, n2);
+    printf("--------------------------\n");
+    print_arr(arr2, 1, n2);
+    printf("--------------------------\n");*/
+    time = clock();
+    check_arr(arr, &res[k].sum, &res[k].count, n1, n2, p, k);
+    
+    for(i = 1; i < p; i++) {
+        leni1 = n1 / p + (i - 1 < l ? 1 : 0);
+        leni = n1 / p + (i < l ? 1 : 0);
+        firsti = (i < l ? i * leni : i * leni + l);
+        lasti = (i - 1 < l ? (i - 1) * leni1 : (i - 1) * leni1 + l) + leni1 - 1;
+        if (leni1 > 1) {
+            if (lasti < n1 - 1 && lasti > 0)
+                for(j = 1; j < n2 - 1; j++) {
+                    mean0 = (arr[lasti * n2 + j + 1] + arr[(lasti - 1) * n2 + j] + arr[lasti * n2 + j - 1]+ arr[(lasti + 1) * n2 + j]) / 4.;  
+                    if ((arr[lasti * n2 + j] > mean0) || (fabs(arr[lasti * n2 + j] - mean0) < EPS)) {
+                        count++;
+                        mean += arr[lasti * n2 + j];
+                    }
+                }
+        }
+        if (firsti < n1 - 1 && firsti > 0) {
+            for(j = 1; j < n2 - 1; j++) {
+                mean0 = (arr[firsti * n2 + j + 1] + arr[(firsti - 1) * n2 + j] + arr[firsti * n2 + j - 1] + arr[(firsti + 1) * n2 + j] ) / 4.;  
+                if ((arr[firsti * n2 + j] > mean0) || (fabs(arr[firsti * n2 + j] - mean0) < EPS)) {
+                    count++;
+                    mean += arr[firsti * n2 + j];
                 }
             }
         }
     }
-
-    if (err) return 0;
-
-    for(i = 0; i < p; i++) 
-        sum += r[i].sum;
-
-    for(i = 0; i < p; i++) 
-        count += r[i].count;
-    
+    reduce_sum(p);
+    for(i = 0; i < p; i++) {
+        mean += res[i].sum;
+        count += res[i].count;
+    }
     if(count == 0) {
         if(k == 0) {
-            a->Res->status = 0;
-            a->Res->count = 0;
-            a->Res->answer = 0;
+            a->res->err = -2;
+            a->count = 0;
         }
         reduce_sum(p);
         return 0;
     }
-    mean = sum/count;
-    r[k].status = f2(name,mean,&r[k].count);
-    w = r[k].count;
-    reduce_sum(p, &w, 1);
-    err = 0;
-    for(i = 0; i < p; i++) {
-        if (r[i].status != 0) {
-            err++;
-            if (k == 0) {
-                switch (r[i].status) {
-                    case -1:
-                        printf("Cannot open file %s!\n", a[k].name);
-                        break;
-                    case -2:
-                        printf("Cannot read file %s!\n", a[k].name);
-                        break;
-                    default:
-                        printf("Unknown error! %s\n", a[k].name);
+    val = mean/count;
+    a -> count = count;
+
+    change_arr(arr, arr1, arr2, val, n1, n2, p, k, res);
+    reduce_sum(p);
+    for(i = 1; i < p; i++) {
+        arr10 = res[i - 1].arr1;
+        arr20 = res[i].arr2;
+        leni1 = n1 / p + (i - 1 < l ? 1 : 0);
+        leni = n1 / p + (i < l ? 1 : 0);
+        firsti = (i < l ? i * leni : i * leni + l);
+        lasti = (i - 1 < l ? (i - 1) * leni1 : (i - 1) * leni1 + l) + leni1 - 1;
+        //printf("\n%d 1st %d     last %d\n", i, firsti, lasti);
+        
+        if (leni1 > 1) {
+            if (lasti < n1 - 1 && lasti > 0)
+                for(j = 1; j < n2 - 1; j++) {
+                    mean0 = (arr[lasti * n2 + j + 1] + arr10[j]  + arr[(lasti + 1) * n2 + j] + arr[lasti * n2 + j - 1]) / 4.;  
+                    if ((arr[lasti * n2 + j] > mean0) || (fabs(arr[lasti * n2 + j] - mean0) < EPS)) {
+                        arr[lasti * n2 + j] = val;
+                    }
+                }
+        }
+        if (firsti < n1 - 1 && firsti > 0) {
+            for(j = 1; j < n2 - 1; j++) {
+                mean0 = (arr[firsti * n2 + j + 1] + arr[(firsti - 1) * n2 + j] + arr20[j] + arr[firsti * n2 + j - 1]) / 4.;  
+                if ((arr[firsti * n2 + j] > mean0) || (fabs(arr[firsti * n2 + j] - mean0) < EPS)) {
+                    arr[firsti * n2 + j] = val;
                 }
             }
         }
     }
-    if(k == 0){
-        a->Res->status = err;
-    }
-    if(err) return 0;
-    //w = r[k].count;
-    //reduce_sum(p, &w, 1);
-    if (k == 0) {
-        a->Res->answer = (int)w;
-    }
-
-    //reduce_sum(p,&(double)r[k].count, 1);
-    //if(k == 0) {
-        //a->Res->answer = r[k].count;
-    //}
-    return 0;
-}*/
+    time = (clock() - time) / CLOCKS_PER_SEC;
+    reduce_sum(p);
+    a -> time = time;
+    delete []arr1;
+    delete []arr2;   
+    return nullptr;
+}
